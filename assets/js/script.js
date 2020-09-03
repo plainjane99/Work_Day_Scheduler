eac688a133b3427e5f44801f3e96baa106f3000b
Plain Jane <jane99hsu@gmail.com>
2020-09-02 23:35:37 -0700

    added grid for first two hour blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // Global Variables Go Here
 var todayEl = document.getElementById('currentDay');
 
-// get current date
-var currentDateEl = document.createElement("div");
-currentDateEl = moment().format('dddd, MMMM DD, YYYY');
-todayEl.append(currentDateEl);
+var currentDateDiv = $("<div>").addClass("currentDay");
+currentDateDiv = moment().format('dddd, MMMM DD, YYYY');
+todayEl.append(currentDateDiv);
+
+// create event
